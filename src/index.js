4e67215ca2531c8a3e8b0a949005b9e06c756950
Menekam Rudy <menekamrudy@gmail.com>
2023-01-26 16:27:13 +0100

    List structure

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
-import printMe from './print.js';
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+const task1 = {
+  description: 'Work on todo project',
+  completed: true,
+  index: 1,
+};
+const task2 = {
+  description: 'Work with coding partners',
+  completed: false,
+  index: 2,
+};
+const task3 = {
+  description: 'Complete all exercises',
+  completed: false,
+  index: 3,
+};
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const tasks = [task1, task2, task3];
+const taskList = document.querySelector('.listItems');
+const checkboxInput = document.createElement('input');
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+const displayTask = () => {
+  for (let i = 0; i < tasks.length; i += 1) {
+    const list = tasks[i];
+    taskList.innerHTML += `<li class="task flex">
+ <input type="checkbox" id="checked" name="checked" >
+ <label class="label" id="label" for="checked">${list.description}</label>
+</li>`;
+    checkboxInput.checked = true;
+  }
+};
 
-  element.appendChild(btn);
-
-  return element;
-}
-
-document.body.appendChild(component());
+window.onload = displayTask();
